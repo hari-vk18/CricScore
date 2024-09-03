ab00d56235de59b6c37d5c7f6d52f119ddf90f76
hari-vk18 <140179088+hari-vk18@users.noreply.github.com>
2024-09-03 19:16:00 +0530

    Created successfully and styled perfectly

--- src/component/International.jsx
+++ src/component/International.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Series from './Series'
+import Card from './Card';
+import './css/International.css'
 
-export default function International({ MatchesTypes }) {
+export default function International({ MatchesTypes, recentMatches }) {
+    const [match, setMatch] = useState([]);
+    useEffect(() => {
+        const match = recentMatches.filter(item => item.matchType === MatchesTypes);
+        setMatch(match);
+        console.log("filtered data", match)
+    }, [MatchesTypes, recentMatches])
     return (
-        <div>
-            <p>InterNational</p>
+        <div className='Type'>
             {
-                MatchesTypes.map((match, index) =>
+                match.map((match, index) =>
                 (
-                    match.seriesAdWrapper && (
-                        <Series key={index} series={match.seriesAdWrapper} />
-                    )
+                    match?.matchType &&
+                    match?.seriesMatches?.map((data, index) => (
+                        data.seriesAdWrapper && (
+                            data.seriesAdWrapper?.matches?.map((match) => (
+
+                                <Card key={index} id={match?.matchInfo?.matchId} matchOb={match} series={data.seriesAdWrapper?.seriesId} />
+
+                            ))
+                        )
+
+                    ))
                 ))
 
             }

b4970b70ff1f7f6f0f1ce418444a7e0409b2ea12
hari-vk18 <140179088+hari-vk18@users.noreply.github.com>
2024-09-03 18:16:02 +0530

    Added button for type

--- src/component/GetMatches.jsx
+++ src/component/GetMatches.jsx
@@ -1,25 +1,53 @@
 import React, { useEffect, useState } from 'react'
-import SearchIcon from "@mui/icons-material/Search";
+import { Button, styled } from '@mui/material';
 import Cricscore from './Cricscore'
-import { Link } from 'react-router-dom';
-import { getLiveMatchInfo, getMatchInfo, getMatchInfo1, getRecentMatchInfo } from '../api/api'
-import ListCard from './ListCard';
+import { getLiveMatchInfo, getRecentMatchInfo } from '../api/api'
 import './css/ListCard.css'
-import Skeleton from './Skeleton'
+
+const navigationOptions = [
+    { label: 'International' },
+    { label: 'League' },
+    { label: 'Domestic' },
+    { label: 'Womens' },
+]
+
+const MatchType = styled(Button)(({ selected }) => ({
+    padding: '5px 5px',
+    borderRadius: '5px',
+    margin: '5px',
+    background: selected
+        ? 'linear-gradient(292deg, rgba(209,55,71,1) 4%, rgba(216,92,77,1) 54%, rgba(230,122,75,1) 90%)'
+        : '#f0f0f0',
+    color: selected ? 'white' : 'black',
+    '&:hover': {
+        background: selected
+            ? 'linear-gradient(292deg, rgba(209,55,71,1) 4%, rgba(216,92,77,1) 54%, rgba(230,122,75,1) 90%)'
+            : '#e0e0e0',
+        color: 'black',
+    },
+    textTransform: 'none',
+    display: 'flex',
+    justifyContent: 'center', // Center text inside the button
+    alignItems: 'center',
+    boxSizing: 'border-box', // Ensures padding and border are included in the element's width and height
+    minWidth: '120px',
+}))
+
+const ButtonContainer = styled('div')({
+    width: '80%',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginBottom: '10px',
+});
 
 function GetMatches() {
-    const [matches, setMatches] = useState([])
     const [limit, setLimit] = useState([]);
     const [search, setSearch] = useState("");
     const [recentMatches, setRecentMatches] = useState([])
+    const [selectedOption, setSelecterOption] = useState('International')
+
     // const [loading, setLoading] = useState(true);
-    const matchType = matches.filter((match) => {
-        return (
-            (match.matchType && match.matchType.toLowerCase().includes(search.toLowerCase())) ||
-            (match.teams && match.teams[0] && match.teams[0].toLowerCase().includes(search.toLowerCase())) ||
-            (match.teams && match.teams[1] && match.teams[1].toLowerCase().includes(search.toLowerCase()))
-        );
-    });
 
     useEffect(() => {
         console.log('From GetMatch....')
@@ -62,7 +90,18 @@ function GetMatches() {
                         placeholder="Search" />
                 </div>
             </div> */}
-
+            <ButtonContainer >
+                {
+                    navigationOptions.map((Option, index) => (
+                        <MatchType
+                            key={index}
+                            selected={selectedOption === Option.label}
+                            onClick={() => setSelecterOption(Option.label)}>
+                            {Option.label}
+                        </MatchType>
+                    ))
+                }
+            </ButtonContainer>
 
             {/* {
                 // loading ? (
